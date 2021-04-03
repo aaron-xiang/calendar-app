6474c8da6b5f6eacc0a6a66810b955cb6e756923
Aaron Xiang <aaron.xiang@gmail.com>
2021-04-03 11:02:55 -0700

    updated _app

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,9 @@ function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
 
-MyApp.getInitialProps = async (appContext) => {
-  const appProps = await App.getInitialProps(appContext);
-  return { ...appProps };
-};
+// MyApp.getInitialProps = async (appContext) => {
+//   const appProps = await App.getInitialProps(appContext);
+//   return { ...appProps };
+// };
 
 export default MyApp;
